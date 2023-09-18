5f8efde81c7b3e903c3d2648d690a1c3a85f6057
xinpengwoo <xinpengwoo@gmail.com>
2023-09-18 05:26:01 +0100

    test3c formal version

--- README.md
+++ README.md
@@ -1,49 +1,70 @@
-# BlurHand
-This repository is an official implementation of the paper **"Recovering 3D Hand Mesh Sequence from a Single Blurry Image: A New Dataset and Temporal Unfolding** (CVPR 2023)". [[arxiv](https://arxiv.org/abs/2303.15417)]
+# Installation
 
-![alt text](assets/images/BlurHandNet_model.png)
-
-![Alt text](BlurHandNet_demo.gif) ![](assets/images/BlurHandNet_demo.gif)
-<div align="center">
-(Left: Blurry input, Right: BlurHandNet output)
-</div>
-
-## BlurHand
-
-You can download our BlurHand dataset in the following [**link**](https://drive.google.com/drive/folders/178q3oUQrOIJMKi0KHoRoQmWRGM8JZnMi?usp=share_link).
-
-Instruction on our dataset is available at [**here**](assets/docs/BlurHand.md).
-
-
-## BlurHandNet
-
-Please follow the below instructions.
-
-1. [**Installation and Dataset preparation**](assets/docs/Installation.md).
-2. [**Training the BlurHandNet**](assets/docs/Training.md) (skip the process if you only want to run a pre-trained model).
-3. [**Testing**](assets/docs/Testing.md).
-
-## Citation
-
-If our work was helpful for your research, please cite our paper.
+1. Set the environment. We recommend using [**Anaconda**](https://www.anaconda.com/products/distribution).
 
 ```
-@inproceedings{oh2023BlurHand,
-  title={Recovering 3D Hand Mesh Sequence from a Single Blurry Image: A New Dataset and Temporal Unfolding},
-  author={Oh, Yeounguk and Park, JoonKyu and Kim, Jaeha and Moon, Gyeongsik and Lee, Kyoung Mu},
-  booktitle={Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition},
-  year={2023}
-}
+ conda create -n BSHN python=3.8
+ conda activate BSHN
+ pip install -r requirements.txt
+ python src/setup.py
+```
+
+2. Modify the `torchgeometry` following [**this**](https://github.com/mks0601/I2L-MeshNet_RELEASE/issues/6#issuecomment-675152527). Without modification, you will meet `RuntimeError: Subtraction, the - operator...`.
+
+3. Download the MANO model files (```mano_v1_2.zip```) from [**MANO website**](https://mano.is.tue.mpg.de/). Unzip and put ```mano_v1_2/models``` into ```src/utils/MANO```. To be specific, ```MANO_RIGHT.pkl``` and ```MANO_LEFT.pkl``` should be located in ```src/utils/MANO/models/MANO_RIGHT.pkl``` and ```src/utils/MANO/models/MANO_LEFT.pkl```, respectively.
+
+# Dataset
+
+Download the following datasets, then locate them in ```datasets```. Instruction is available at [**here**](assets/docs/BlurHand.md).
+
++ [BlurHand](https://drive.google.com/drive/folders/178q3oUQrOIJMKi0KHoRoQmWRGM8JZnMi)
++ [InterHand2.6M](https://mks0601.github.io/InterHand2.6M/)
+
+For the generation of our BlurSegHand dataset, please run the below command.
+
+```
+CUDA_VISIBLE_DEVICES=0,1 python src/data/BlurSegMask_generation.py -opt options/train/BlurHandNet_BH.yml
+CUDA_VISIBLE_DEVICES=0,1 python src/data/BlurSegMask_generation.py -opt options/test/BlurHandNet_BH.yml
+```
+
+Our BlurSegHand dataset shares the same structure of [BlurHand](https://github.com/xinpengwoo/BlurSegHand/blob/main/assets/docs/BlurHand.md).
+
+# Training
+
+For training our BlurSegHandNet on BlurSegHand, please run the below command.
+
+```
+CUDA_VISIBLE_DEVICES=0,1 python src/train.py -opt options/train/BlurHandNet_BH.yml
+```
+
+You can check detailed training configurations in `options/train/BlurHandNet_BH.yml`.
+
+In default, we used 2 GPUs, but you can change the configurations by modulating `num_gpus` in `.yml` file.
+
+The training states and logs will be saved in `experiments/BlurHandNet_BH`.
+
+
+# Testing
+
+If you trained your own BlurSegHandNet following, please run the below command to test your model.
+
+```
+CUDA_VISIBLE_DEVICES=0 python src/test.py -opt options/test/BlurHandNet_BH.yml
+```
+
+If you want to inference the model on one image input, please run the below command.
+
+```
+CUDA_VISIBLE_DEVICES=0 python src/test.py -opt options/test/inference.yml
 ```
 
 ## Acknowledgement
 
 Our code implementaions are motivated from the below codes. We thank the authors for sharing the awesome repositories.
 
-- [Hand4Whole_RELEASE](https://github.com/mks0601/Hand4Whole_RELEASE)
-- [maed](https://github.com/ziniuwan/maed)
-- [BasicSR](https://github.com/XPixelGroup/BasicSR)
+- [BlurHand_RELEASE](https://github.com/JaehaKim97/BlurHand_RELEASE)
+- [digit-interacting](https://github.com/zc-alexfan/digit-interacting)
 
 
 ## Contact
-If you have any question, please email `jhkim97s2@gmail.com`.
+If you have any question, please email `xinpengwoo@gmail.com`.
---
+++ src/data/BlurSegMask_generation.py
@@ -0,0 +1,144 @@
+import cv2
+import os
+import numpy as np
+import os.path as osp
+import torch
+import concurrent.futures
+
+from tqdm import tqdm
+from pycocotools.coco import COCO
+from utils.options import parse_options, copy_opt_file
+
+
+def main():    
+    # load opt and args from yaml
+    opt, args = parse_options()
+    
+    # dynamic dataset import
+    opt_data = opt['dataset']
+    dataset_name = opt_data['name']
+    exec(f'from data.{dataset_name} import {dataset_name}')
+    globals().update(locals())
+    # train and test dataset
+    tester = Tester(opt, opt_data, "train")
+    data = tester.datalist
+
+    process_with_tqdm(28, data)
+    # for image_paths in tqdm(data):
+    #     process_each_dict(image_paths)
+
+def process_with_tqdm(max_threads, data_dicts):
+    with tqdm(total=len(data_dicts), position=0, leave=True) as pbar:
+        with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
+            # Function to process a single dictionary with progress update
+            def process_single(data_dict):
+                process_dict(data_dict)
+                pbar.update(1)
+
+            # Submit the dictionaries for processing concurrently
+            futures = [executor.submit(process_single, data_dict) for data_dict in data_dicts]
+
+            # Wait for all tasks to complete
+            concurrent.futures.wait(futures)
+
+def process_dict(image_paths):
+    blurry_image_path = image_paths.pop('blurry_image_path')
+    sharp_images_path = [value for key, value in image_paths.items()]
+    # get variance path
+    blurry_image_path_parts = blurry_image_path.split(".")
+    blurry_image_path_parts[-2] += '-var'
+    variance_path = ".".join(blurry_image_path_parts)
+    cacuclate_variance(sharp_images_path, variance_path)
+    #print(variance_path + " is successful")
+
+def cacuclate_variance(data,var_path):
+    # Read the first image to get the image size
+    image_path = data[0]
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    height, width, _ = image.shape
+
+    # Create an empty array to store pixel values for all images
+    pixel_values = np.zeros((height, width, 3, len(data)-1), dtype=np.float32)
+
+    # Read and process each image
+    for i in range(len(data)-1):
+        image_path = data[i]
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        pixel_values[:, :, :, i] = image.astype(np.float32) #/ 255.0  # Normalize pixel values to [0, 1]
+        
+    variance = np.var(pixel_values, axis=3)
+    cv2.imwrite(var_path, np.uint8(variance))
+
+    # difference
+    # blurry_40 = cv2.imread("./blurry_33_l1.png", cv2.IMREAD_COLOR)
+    # generated = blurry_40.astype(np.float32)
+    # blurry = cv2.imread(images_path+"/blur_image22396.png", cv2.IMREAD_COLOR)
+    # gt = blurry.astype(np.float32)
+    # result = generated - gt
+    # #cv2.imwrite("difference.png", np.uint8(result))
+    # print("max_value:",np.max(result))
+    # print("min_value:",np.min(result))
+    # print("is Same?", np.all(result == 0))
+    # # caculate the difference
+    # diff = cv2.absdiff(generated, gt)
+    # diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+    # # apply threshold to get difference image
+    # _, thresholded = cv2.threshold(diff_gray, 3, 255, cv2.THRESH_BINARY)
+
+    # # visualize difference image
+    # cv2.imwrite("Difference_l1.png", diff)
+    # cv2.imwrite("Thresholded Difference_l1.png", thresholded)
+
+
+class Tester(torch.utils.data.Dataset):
+    def __init__(self, opt, opt_data, data_split):
+        self.opt = opt
+        self.opt_params = opt['task_parameters']
+        self.data_split = 'train' if data_split == 'train' else 'test'
+
+        # path for images and annotations
+        self.img_path = opt_data['img_path']
+        self.annot_path = opt_data['annot_path']
+
+        # path for sharp images path
+        self.sharp_img_path = "/home/hcvl/Xinpeng/InterHand3.6M/InterHand2.6M_30fps_batch1/images"
+
+        # data loader and construct batch generator
+        opt_data = self.opt['dataset']
+        
+        dataset_name = opt_data['name']
+        self.datalist = self.load_data()
+
+    def load_data(self):
+        db = COCO(osp.join(self.annot_path, self.data_split, f'BlurHand_{self.data_split}_data.json'))
+        num = 0
+        datalist = []
+        for aid in [*db.anns.keys()]:
+            ann = db.anns[aid]
+            if not ann['is_middle']:
+                continue
+            image_id = ann['image_id']
+            img = db.loadImgs(image_id)[0]
+            img_path = osp.join(self.img_path, self.data_split, img['file_name'])
+            # repeat the process for past(1st) image
+            data = {}
+            data['blurry_image_path'] = img_path
+            sharp_aid_list = ann['aid_list'][:]
+            for i in range(len(sharp_aid_list)):
+                aid = sharp_aid_list[i]
+                ann = db.anns[aid]
+                image_id = ann['image_id']
+                img = db.loadImgs(image_id)[0]
+                # sharp_img_path, png -> jpg
+                file_name_parts = img['file_name'].split(".")
+                file_name_parts[-1] = "jpg"
+                img_path = osp.join(self.sharp_img_path, self.data_split, ".".join(file_name_parts))
+                data[f'{i + 1}_sharp_image_path'] = img_path
+            datalist.append(data)
+        print("Total number of sample in BlurHand: {}".format(len(datalist)))
+        return datalist
+    
+    
+if __name__ == "__main__":
+    main()
